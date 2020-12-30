bdca03c8e72315053e45baaba2f34a4cacf48297
elenachurilova <ellena.churilova@gmail.com>
2020-12-30 12:03:18 -0800

    Completed CoffeeMachine

--- main.py
+++ main.py
@@ -57,24 +57,19 @@ while True:
         given_dollar_amount = 0
         if enough_ingredients:
             print("Please insert coins.")
+
             quarters = int(input("how many quarters?: "))
             dimes = int(input("how many dimes?: "))
             nickles = int(input("how many nickles?: "))
             pennies = int(input("how many pennies?: "))
-
-            # count how much money was given
             given_dollar_amount = (quarters * 0.25) + (dimes * 0.1) + (nickles * 0.05) + (pennies * 0.01)
 
-            # compare to what the cost of the drink is:
-            # if more or enough was given, put money in the register, give change if any
             if given_dollar_amount >= MENU[answer]["cost"]:
                 change = given_dollar_amount - MENU[answer]["cost"]
                 resources["money"] += given_dollar_amount - change
                 if change > 0:
                     print(f"Here is ${round(change, 2)} in change")
                 print(f"Here's your {answer} ☕️ Enjoy!")
-
-            # if less was given, refund the money don't make the drink
             else:
                 print("Sorry that's not enough money. Money refunded.")
 
